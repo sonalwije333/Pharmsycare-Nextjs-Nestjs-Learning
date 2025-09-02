feac72163fbce3a8cadeabc9b54018a4d4e487cb
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-09-02 19:58:22 +0530

    feat: Coded Analytics Module

--- apps/server/src/modules/analytics/analytics.module.ts
+++ apps/server/src/modules/analytics/analytics.module.ts
@@ -1,19 +1,36 @@
 import { Module } from '@nestjs/common';
-// import { AnalyticsService } from './analytics.service';
-// import {
-//   AnalyticsController,
-//   CategoryWiseProductController,
-//   LowStockProductsController,
-//   TopRateProductController,
-// } from './analytics.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AnalyticsService } from './analytics.service';
+import {
+  AnalyticsController,
+  CategoryWiseProductController,
+  LowStockProductsController,
+  TopRateProductController,
+} from './analytics.controller';
+import { Analytics, TotalYearSaleByMonth } from './entities/analytics.entity';
+import { CategoryWiseProduct } from './entities/category-wise-product.entity';
+import { TopRateProduct } from './entities/top-rate-product.entity';
+import { Product } from '../products/entities/product.entity';
+import { Attachment } from '../common/entities/attachment.entity';
 
 @Module({
-  // controllers: [
-  //   AnalyticsController,
-  //   CategoryWiseProductController,
-  //   LowStockProductsController,
-  //   TopRateProductController,
-  // ],
-  // providers: [AnalyticsService],
+  imports: [
+    TypeOrmModule.forFeature([
+      Analytics,
+      TotalYearSaleByMonth,
+      CategoryWiseProduct,
+      TopRateProduct,
+      Product,
+      Attachment,
+    ]),
+  ],
+  controllers: [
+    AnalyticsController,
+    CategoryWiseProductController,
+    LowStockProductsController,
+    TopRateProductController,
+  ],
+  providers: [AnalyticsService],
+  exports: [AnalyticsService],
 })
 export class AnalyticsModule {}
