72f01c4ab1f5d2eeed5f838fd9dc6396d52cfef1
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-09-25 10:19:25 +0530

    feat: Coded Product Module

--- apps/server/src/modules/products/dto/get-best-selling-products.dto.ts
+++ apps/server/src/modules/products/dto/get-best-selling-products.dto.ts
@@ -1,5 +1,19 @@
+import {ApiPropertyOptional} from "@nestjs/swagger";
+import {IsNumber, IsOptional, IsString} from "class-validator";
+
 export class GetBestSellingProductsDto {
-  type_slug?: string;
-  limit: number;
-  shop_id?: number;
+    @ApiPropertyOptional({ description: 'Type slug', example: 'electronics' })
+    @IsOptional()
+    @IsString()
+    type_slug?: string;
+
+    @ApiPropertyOptional({ description: 'Limit', example: 10, default: 10 })
+    @IsOptional()
+    @IsNumber()
+    limit?: number;
+
+    @ApiPropertyOptional({ description: 'Shop ID', example: 1 })
+    @IsOptional()
+    @IsNumber()
+    shop_id?: number;
 }
--- apps/server/src/modules/products/products.module.ts
+++ apps/server/src/modules/products/products.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { Type } from '../types/entities/type.entity';
+import { Shop } from '../shops/entites/shop.entity';
+import { Tag } from '../tags/entities/tag.entity';
+import { ShippingClass } from '../shippings/entities/shipping-class.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { Type } from '../types/entities/type.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Type])], // 👈 register entity here
-  controllers: [ProductsController],
-  providers: [ProductsService],
-  exports: [ProductsService], // if other modules need this service
+    imports: [TypeOrmModule.forFeature([Product, Type, Shop, Tag, ShippingClass])],
+    controllers: [ProductsController],
+    providers: [ProductsService],
+    exports: [ProductsService],
 })
 export class ProductsModule {}
