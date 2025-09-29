56e19b4d32f35c21b38bd950c1e74ee7c1a8461b
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-09-30 02:44:34 +0530

    Update enums.ts

--- apps/server/src/common/enums/enums.ts
+++ apps/server/src/common/enums/enums.ts
@@ -207,4 +207,54 @@ export enum ProductType {
     SIMPLE = 'simple',
     VARIABLE = 'variable'
 }
+//orders
+export enum OrderStatusType {
+  PENDING = 'order-pending',
+  PROCESSING = 'order-processing',
+  COMPLETED = 'order-completed',
+  CANCELLED = 'order-cancelled',
+  REFUNDED = 'order-refunded',
+  FAILED = 'order-failed',
+  AT_LOCAL_FACILITY = 'order-at-local-facility',
+  OUT_FOR_DELIVERY = 'order-out-for-delivery',
+}
+export enum QueryOrderStatusesOrderByColumn {
+  CREATED_AT = 'CREATED_AT',
+  NAME = 'NAME',
+  UPDATED_AT = 'UPDATED_AT',
+  SERIAL = 'SERIAL',
+}
 
+export enum QueryOrdersOrderByColumn {
+  CREATED_AT = 'CREATED_AT',
+  UPDATED_AT = 'UPDATED_AT',
+  TOTAL = 'TOTAL',
+  TRACKING_NUMBER = 'TRACKING_NUMBER',
+}
+
+export enum QueryOrderFilesOrderByColumn {
+  CREATED_AT = 'CREATED_AT',
+  NAME = 'NAME',
+  UPDATED_AT = 'UPDATED_AT',
+}
+
+//PAYEMNT
+export enum PaymentGatewayType {
+  STRIPE = 'STRIPE',
+  CASH_ON_DELIVERY = 'CASH_ON_DELIVERY',
+  CASH = 'CASH',
+  FULL_WALLET_PAYMENT = 'FULL_WALLET_PAYMENT',
+  PAYPAL = 'PAYPAL',
+  RAZORPAY = 'RAZORPAY',
+}
+export enum PaymentStatusType {
+  PENDING = 'payment-pending',
+  PROCESSING = 'payment-processing',
+  SUCCESS = 'payment-success',
+  FAILED = 'payment-failed',
+  REVERSAL = 'payment-reversal',
+  CASH_ON_DELIVERY = 'payment-cash-on-delivery',
+  CASH = 'payment-cash',
+  WALLET = 'payment-wallet',
+  AWAITING_FOR_APPROVAL = 'payment-awaiting-for-approval',
+}
