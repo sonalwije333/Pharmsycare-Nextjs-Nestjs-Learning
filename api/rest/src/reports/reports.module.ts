8492c967097062b8eb39f6ed7e5e6e50cafecaea
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2026-04-30 03:28:18 +0530

    Update reports.module.ts

--- api/rest/src/reports/reports.module.ts
+++ api/rest/src/reports/reports.module.ts
@@ -1,10 +1,11 @@
 // reports/reports.module.ts
 import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller';
+import { MyReportsController } from './my-reports.controller';
 import { ReportsService } from './reports.service';
 
 @Module({
-  controllers: [ReportsController],
+  controllers: [ReportsController, MyReportsController],
   providers: [ReportsService],
 })
 export class ReportsModule {}
